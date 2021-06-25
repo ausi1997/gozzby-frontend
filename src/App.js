b0c9a2c3b59cecf4e4ac78998b68657f87c38170
ausi1997.com <ausafen1018@gmail.com>
2021-06-25 09:23:27 +0530

    react setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
+
 import './App.css';
-import NavBar from './components/NavBar';
 
 function App() {
   return (
-   <div>
-   <NavBar></NavBar>
-   </div>
+    <div className="App">
+    </div>
   );
 }
 
